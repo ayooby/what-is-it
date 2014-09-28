cb1ae4615dddedcc9d719acfe857f4a72615e41f
Rahman Mousavian <mousavian.rahman@gmail.com>
2014-09-29 04:07:05 +0800

    Landing page prototype

---
+++ app/views/layouts/landing-page.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>What is it</title>
+	{{ HTML::style('css/style.css') }}
+	{{ HTML::style('css/responsive.css') }}
+</head>
+<body>
+	<div id="main">
+		<section class="posts">
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-01.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-02.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-03.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-04.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-05.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-06.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-07.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-08.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-09.jpg" alt=""></a></div>
+				</div>
+			</article>
+			<article class="post">
+				<div class="content-wrapper">
+					<div class="post-thumbnail clearfix"><a href="#"><img src="img/p-10.jpg" alt=""></a></div>
+				</div>
+			</article>
+		</section>
+	</div>
+
+
+
+
+
+
+	{{ HTML::script('js/jquery.js') }}
+	{{ HTML::script('js/jquery-migrate.min.js') }}
+	{{ HTML::script('js/jquery.isotope.min.js') }}
+	{{ HTML::script('js/script.js') }}
+	<script type="text/javascript">
+		var $=jQuery;
+		$().ready(function(){
+		});
+	</script>
+</body>
+</html>
